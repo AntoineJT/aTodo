3939784873d45d89505286e37fce2d7eb2d19d0d
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-12-07 11:54:49 +0100

    Add dummy back button to the task view

--- Sources/app/src/main/java/com/github/antoinejt/atodo/TaskActivity.java
+++ Sources/app/src/main/java/com/github/antoinejt/atodo/TaskActivity.java
@@ -1,5 +1,6 @@
 package com.github.antoinejt.atodo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,9 @@ public class TaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
