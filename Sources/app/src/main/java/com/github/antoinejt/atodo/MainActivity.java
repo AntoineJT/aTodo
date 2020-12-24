114fd46ef1a1f16eae4ce2582bbed30d03e016cc
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-12-24 12:31:56 +0100

    FloatingButton now opens TaskActivity

--- Sources/app/src/main/java/com/github/antoinejt/atodo/MainActivity.java
+++ Sources/app/src/main/java/com/github/antoinejt/atodo/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -38,10 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> {
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-        });
+        fab.setOnClickListener(view -> changeActivity(TaskActivity.class));
     }
 
     @Override
