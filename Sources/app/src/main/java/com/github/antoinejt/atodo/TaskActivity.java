935e9e810d8ca597e6c12bf05694e499701ab411
Fadgiras <mayeuxgudinaflorian@gmail.com>
2021-01-15 23:46:56 +0100

    To Fix : First version of the DB queries

--- Sources/app/src/main/java/com/github/antoinejt/atodo/TaskActivity.java
+++ Sources/app/src/main/java/com/github/antoinejt/atodo/TaskActivity.java
@@ -3,9 +3,17 @@ package com.github.antoinejt.atodo;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class TaskActivity extends AppCompatActivity {
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+
+public class TaskActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +21,39 @@ public class TaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task);
 
         ActionBar actionBar = getSupportActionBar();
-        assert actionBar != null;
+        Objects.requireNonNull(actionBar);
         actionBar.setDisplayHomeAsUpEnabled(true);
+        Button bt1 = findViewById(R.id.buttonOkay);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Db add + return
+                task();
+            }
+        });
+    }
+
+    private void task(){
+        //TODO Verify getApplicationContext / getBaseContext
+        DBUtils db = new DBUtils(this.getApplicationContext());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        String currentDate = sdf.format(new Date());
+
+        EditText editTaskName = (EditText) findViewById(R.id.fieldTaskName);
+        String taskName = editTaskName.getText().toString();
+
+        EditText editTaskEnd = (EditText) findViewById(R.id.fieldTaskEnd);
+        String taskEnd = editTaskEnd.getText().toString();
+
+        EditText editTaskDescription = (EditText) findViewById(R.id.fieldTaskDescription);
+        String taskDescription = editTaskDescription.getText().toString();
+
+        db.createTaskList(taskName, currentDate, taskEnd);
+        db.insertTask(taskName,taskDescription, taskEnd);
+    }
+
+    @Override
+    public void onClick(View v) {
     }
 }
