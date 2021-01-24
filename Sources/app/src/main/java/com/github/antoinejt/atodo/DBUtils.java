aba4c6e9e8e695d660de69428ae3589e19ab060b
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-01-24 14:01:39 +0100

    Save new tasks to database

--- Sources/app/src/main/java/com/github/antoinejt/atodo/DBUtils.java
+++ Sources/app/src/main/java/com/github/antoinejt/atodo/DBUtils.java
@@ -5,48 +5,49 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.provider.Settings;
 
 import com.github.antoinejt.atodo.dataclasses.TaskItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DBUtils extends SQLiteOpenHelper{
+public class DBUtils extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Database.db";
+    private final SQLiteDatabase wdb;
 
     public DBUtils(Context context) {
         super(context, DATABASE_NAME , null, 1);
+        this.wdb = getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(
-                String.format("create table Task (" +
-                        "taskId integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                        "taskName text NOT NULL," +
-                        "taskDescription text NOT NULL," +
-                        "taskListId text NOT NULL"+
-                        "taskFinished INTEGER NOT NULL DEFAULT 0 CHECK(taskFinished IN (0,1)))")
+                "CREATE TABLE Task (" +
+                        "taskId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                        "taskName TEXT NOT NULL," +
+                        "taskDescription TEXT NOT NULL," +
+                        "taskListId TEXT NOT NULL," +
+                        "taskFinished INTEGER NOT NULL DEFAULT 0 CHECK(taskFinished IN (0, 1)))"
         );
 
         db.execSQL(
-                String.format("create table TaskList (" +
-                        "tasklistId integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                        "tasklistName text NOT NULL," +
-                        "taskListCreatedAt datetime NOT NULL," +
-                        "taskListEnds datetime NOT NULL )")
+                "CREATE TABLE TaskList (" +
+                        "tasklistId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                        "tasklistName TEXT NOT NULL," +
+                        "taskListCreatedAt DATETIME NOT NULL," +
+                        "taskListEnds DATETIME NOT NULL)"
         );
         db.execSQL(
-                String.format("create table Category(" +
-                        "categoryId integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                        " categoryName text," +
-                        " categoryDescription text)")
+                "CREATE TABLE Category(" +
+                        "categoryId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                        "categoryName TEXT, " +
+                        "categoryDescription TEXT)"
         );
         db.execSQL(
-                String.format("create table Alert(" +
-                        "alertId integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                        " alertsAt datetime)")
+                "CREATE TABLE Alert(" +
+                        "alertId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                        "alertsAt DATETIME)"
         );
     }
 
@@ -60,116 +61,104 @@ public class DBUtils extends SQLiteOpenHelper{
     }
 
     public boolean createTaskList(String name, String createdAt, String end){
-        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("taskListName", name);
         contentValues.put("taskListCreatedAt", createdAt);
         contentValues.put("taskListEnds", end);
-        db.insert("TaskList", null, contentValues);
+        wdb.insert("TaskList", null, contentValues);
         return true;
     }
     //TODO Review this insert thing, Task insert broken AF
     public String idSelect(String name){
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "SELECT taskListId FROM Tasklist WHERE taskListName = ?", null );
+        Cursor res =  wdb.rawQuery( "SELECT taskListId FROM Tasklist WHERE taskListName = ?", null );
         System.err.print(res.toString());
         return res.toString();
     }
 
     public void miniInsert(String taskName, String taskDescription, String taskFinished, String taskListId){
-        SQLiteDatabase db = this.getWritableDatabase();
         //db.rawQuery("INSERT INTO Task (TaskName, TaskDescription, TaskFinished) VALUES (?, ?, ?) WHERE TaskList.taskListId = ?", null);
-        db.rawQuery("INSERT INTO Task (taskName, taskDescription, taskFinished) VALUES (?, ?, ?, ?)", null);
+        wdb.rawQuery("INSERT INTO Task (taskName, taskDescription, taskFinished) VALUES (?, ?, ?)",
+                new String[] { taskName, taskDescription, taskFinished});
     }
 
+    // imo we must keep the taskListId in the activity's data to pass it by parameter here
+    // to avoid doing an error-prone request (we can have 2 taskList named the same)
     public boolean insertTask(String taskName, String taskDescription, String taskFinished) {
-        String listId;
-        listId= idSelect(taskName);
+        String listId = idSelect(taskName);
         miniInsert(taskName, taskDescription, taskFinished, listId);
         return true;
     }
 
     public boolean createCategory(String categoryName, String categoryDescription){
-        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("categoryName", categoryName);
         contentValues.put("categoryDescription", categoryDescription);
-        db.insert("Category", null, contentValues);
+        wdb.insert("Category", null, contentValues);
         return true;
     }
 
     public boolean createAlert(String alertsAt){
-        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("alertsAt", alertsAt);
-        db.insert("Alert", null, contentValues);
+        wdb.insert("Alert", null, contentValues);
         return true;
     }
 
 
 
     public Integer deleteTaskList(Integer id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete("TaskList", "tasklistId = ? ", new String[] { Integer.toString(id) });
+        return wdb.delete("TaskList", "tasklistId = ? ", new String[] { Integer.toString(id) });
     }
 
     public Integer deleteCategory(Integer id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete("Category", "categoryId = ? ", new String[] { Integer.toString(id) });
+        return wdb.delete("Category", "categoryId = ? ", new String[] { Integer.toString(id) });
     }
 
     public Integer deleteAlert(Integer id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete("Alert", "alertId = ? ", new String[] { Integer.toString(id) });
+        return wdb.delete("Alert", "alertId = ? ", new String[] { Integer.toString(id) });
     }
 
     public Integer deleteTask(Integer id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete("Task", "taskId = ? ", new String[] { Integer.toString(id) });
+        return wdb.delete("Task", "taskId = ? ", new String[] { Integer.toString(id) });
     }
 
 
 
     public boolean updateTask(int id, String taskName, String taskDescription, String taskFinished) {
-        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("taskName", taskName);
         contentValues.put("taskDescription", taskDescription);
         contentValues.put("taskFinished", taskFinished);
-        db.update("Task", contentValues, "taskId = ? ", new String[] { Integer.toString(id) });
+        wdb.update("Task", contentValues, "taskId = ? ", new String[] { Integer.toString(id) });
         return true;
     }
 
     public boolean updateTaskList(int id, String taskName, String taskListCreatedAt, String end) {
-        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("taskListName", taskName);
         contentValues.put("taskListCreatedAt", taskListCreatedAt);
         contentValues.put("taskListEnds", end);
-        db.update("TaskList", contentValues, "TaskList.tasklistId = ? ", new String[] { Integer.toString(id) });
+        wdb.update("TaskList", contentValues, "TaskList.tasklistId = ? ", new String[] { Integer.toString(id) });
         return true;
     }
 
     public boolean updateCategory(int id, String categoryName, String categoryDescription) {
-        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("categoryName", categoryName);
         contentValues.put("categoryDescription", categoryDescription);
-        db.update("Category", contentValues, "categoryId = ? ", new String[] { Integer.toString(id) });
+        wdb.update("Category", contentValues, "categoryId = ? ", new String[] { Integer.toString(id) });
         return true;
     }
 
     public boolean updateAlert(int id, String alertsAt) {
-        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("alertsAt", alertsAt);
-        db.update("Alert", contentValues, "alertId = ? ", new String[] { Integer.toString(id) });
+        wdb.update("Alert", contentValues, "alertId = ? ", new String[] { Integer.toString(id) });
         return true;
     }
 
     public List<TaskItem> getTasks() {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "select taskName, taskDescription from Task", null );
+        Cursor res =  wdb.rawQuery( "SELECT taskName, taskDescription from Task", null );
         res.moveToFirst();
 
         List<TaskItem> taskItems = new ArrayList<>();
--- Sources/app/src/main/java/com/github/antoinejt/atodo/TaskActivity.java
+++ Sources/app/src/main/java/com/github/antoinejt/atodo/TaskActivity.java
@@ -9,12 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.github.antoinejt.atodo.utils.Goto;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
 
 public class TaskActivity extends AppCompatActivity implements View.OnClickListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,34 +25,41 @@ public class TaskActivity extends AppCompatActivity implements View.OnClickListe
         ActionBar actionBar = getSupportActionBar();
         Objects.requireNonNull(actionBar);
         actionBar.setDisplayHomeAsUpEnabled(true);
-        Button bt1 = findViewById(R.id.buttonOkay);
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO Db add + return
-                task();
-            }
+        final AppCompatActivity activity = this;
+        Button okayButton = findViewById(R.id.buttonOkay);
+        okayButton.setOnClickListener(v -> {
+            boolean succeed = createTask();
+            Snackbar.make(v, succeed ? "Task saved correctly" : "Error while saving task", 750)
+                    .addCallback(new Snackbar.Callback() {
+                        @Override
+                        public void onDismissed(Snackbar snackbar, int event) {
+                            super.onDismissed(snackbar, event);
+                            Goto.changeActivity(activity, MainActivity.class);
+                        }}).show();
         });
     }
 
-    private void task(){
+    private boolean createTask(){
         //TODO Verify getApplicationContext / getBaseContext
-        DBUtils db = new DBUtils(this.getApplicationContext());
+        boolean result;
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-        String currentDate = sdf.format(new Date());
+        try (DBUtils db = new DBUtils(this.getApplicationContext())) {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+            String currentDate = sdf.format(new Date());
 
-        EditText editTaskName = (EditText) findViewById(R.id.fieldTaskName);
-        String taskName = editTaskName.getText().toString();
+            EditText editTaskName = (EditText) findViewById(R.id.fieldTaskName);
+            String taskName = editTaskName.getText().toString();
 
-        EditText editTaskEnd = (EditText) findViewById(R.id.fieldTaskEnd);
-        String taskEnd = editTaskEnd.getText().toString();
+            EditText editTaskEnd = (EditText) findViewById(R.id.fieldTaskEnd);
+            String taskEnd = editTaskEnd.getText().toString();
 
-        EditText editTaskDescription = (EditText) findViewById(R.id.fieldTaskDescription);
-        String taskDescription = editTaskDescription.getText().toString();
+            EditText editTaskDescription = (EditText) findViewById(R.id.fieldTaskDescription);
+            String taskDescription = editTaskDescription.getText().toString();
 
-        db.createTaskList(taskName, currentDate, taskEnd);
-        db.insertTask(taskName,taskDescription, taskEnd);
+            result = db.createTaskList(taskName, currentDate, taskEnd) && db.insertTask(taskName,taskDescription, taskEnd);
+        }
+
+        return result;
     }
 
     @Override
