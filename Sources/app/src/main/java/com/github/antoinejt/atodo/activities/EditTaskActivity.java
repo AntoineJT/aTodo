e85e934f4315b75c01ae8603b9329cf22813c69f
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-01-26 19:09:06 +0100

    Make edit feature works

--- Sources/app/src/main/java/com/github/antoinejt/atodo/activities/EditTaskActivity.java
+++ Sources/app/src/main/java/com/github/antoinejt/atodo/activities/EditTaskActivity.java
@@ -13,6 +13,9 @@ import com.github.antoinejt.atodo.utils.DateFormatter;
 import com.github.antoinejt.atodo.utils.Goto;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.text.ParseException;
+import java.util.Date;
+
 // Sonarlint java:S110 - Inheritance tree of classes should not be too deep
 //   I can't do anything about it, that's how Android apps are built.
 @SuppressWarnings("java:S110")
@@ -30,14 +33,40 @@ public class EditTaskActivity extends AppCompatActivity {
 
         final TaskItem item = (TaskItem) getIntent().getSerializableExtra("taskData");
         try (DBUtils db = DBUtils.get(this.getApplicationContext())) {
-            findEditText(R.id.fieldTaskName).setText(item.title);
-            findEditText(R.id.fieldTaskDescription).setText(item.description);
-            findEditText(R.id.fieldTaskEnd).setText(DateFormatter.formatter.format(item.deadline));
-            ((CheckBox) findViewById(R.id.fieldIsTaskFinished)).setChecked(item.isFinished);
+            final EditText nameField = findEditText(R.id.fieldTaskName);
+            final EditText descriptionField = findEditText(R.id.fieldTaskDescription);
+            final EditText deadlineField = findEditText(R.id.fieldTaskEnd);
+            final CheckBox isFinishedField = findViewById(R.id.fieldIsTaskFinished);
+
+            nameField.setText(item.title);
+            descriptionField.setText(item.description);
+            deadlineField.setText(DateFormatter.formatter.format(item.deadline));
+            isFinishedField.setChecked(item.isFinished);
 
             findViewById(R.id.buttonEdit).setOnClickListener(listener -> {
-                // TODO Find a way to transmit the task id here
                 Snackbar.make(listener, "To implement", 500).show();
+                Date newDeadline = null;
+                try {
+                    newDeadline = DateFormatter.formatter.parse(deadlineField.getText().toString());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                final boolean succeed = db.updateTask(item.id, nameField.getText().toString(),
+                        descriptionField.getText().toString(),
+                        newDeadline, isFinishedField.isChecked());
+                final String status = succeed
+                        ? "Task successfully updated!"
+                        : "Error while updating task!";
+
+                final AppCompatActivity activity = this;
+                Snackbar.make(listener, status, 500).addCallback(new Snackbar.Callback() {
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        super.onDismissed(snackbar, event);
+                        Goto.changeActivity(activity, MainActivity.class);
+                    }
+                }).show();
             });
 
             findViewById(R.id.buttonDelete).setOnClickListener(listener -> {
