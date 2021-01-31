d2c3e5b5346bdaef7a429bd015058501c81ea8d1
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-01-31 01:38:05 +0100

    Check the selected language radio button

--- Sources/app/src/main/java/com/github/antoinejt/atodo/activities/ChangeLangActivity.java
+++ Sources/app/src/main/java/com/github/antoinejt/atodo/activities/ChangeLangActivity.java
@@ -3,26 +3,33 @@ package com.github.antoinejt.atodo.activities;
 import android.os.Bundle;
 import android.widget.RadioGroup;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.akexorcist.localizationactivity.ui.LocalizationActivity;
 import com.github.antoinejt.atodo.R;
+import com.github.antoinejt.atodo.utils.BiMap;
 import com.github.antoinejt.atodo.utils.Goto;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Locale;
 
 // Sonarlint java:S110 - Inheritance tree of classes should not be too deep
 //   I can't do anything about it, that's how Android apps are built.
 @SuppressWarnings("java:S110")
 public class ChangeLangActivity extends LocalizationActivity {
-    private static final Map<Integer, String> langCodes;
+    private static final BiMap<Integer, String> langCodes;
 
     static {
-        langCodes = new HashMap<>();
+        langCodes = new BiMap<>();
         langCodes.put(R.id.radio_en, "en");
         langCodes.put(R.id.radio_es, "es");
         langCodes.put(R.id.radio_fr, "fr");
+    }
+
+    private void checkDefaultLanguage(RadioGroup rg) {
+        String defaultLangCode = Locale.getDefault().getLanguage();
+        if (!langCodes.get().containsValue(defaultLangCode)) {
+            // fallback language
+            defaultLangCode = "en";
+        }
+        rg.check(langCodes.inverted().get(defaultLangCode));
     }
 
     @Override
@@ -32,10 +39,12 @@ public class ChangeLangActivity extends LocalizationActivity {
 
         Goto.addBackButton(getSupportActionBar());
 
-        final AppCompatActivity activity = this;
+        final RadioGroup rg = findViewById(R.id.selected_lang);
+
+        checkDefaultLanguage(rg);
+
         findViewById(R.id.btnChangeLangView).setOnClickListener(view -> {
-            RadioGroup rg = findViewById(R.id.selected_lang);
-            String langCode = langCodes.get(rg.getCheckedRadioButtonId());
+            final String langCode = langCodes.get().get(rg.getCheckedRadioButtonId());
             setLanguage(langCode);
         });
     }
