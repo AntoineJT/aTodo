1e5a92a578e7a145622441d4f5f31764ffc4882e
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-01-31 01:06:31 +0100

    Make switch feature works
    NB: The latest set language needs to be
    
    set at launch of the app

--- Sources/app/src/main/java/com/github/antoinejt/atodo/activities/ChangeLangActivity.java
+++ Sources/app/src/main/java/com/github/antoinejt/atodo/activities/ChangeLangActivity.java
@@ -5,9 +5,9 @@ import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.akexorcist.localizationactivity.ui.LocalizationActivity;
 import com.github.antoinejt.atodo.R;
 import com.github.antoinejt.atodo.utils.Goto;
-import com.github.antoinejt.atodo.utils.LocaleHelper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,18 +15,18 @@ import java.util.Map;
 // Sonarlint java:S110 - Inheritance tree of classes should not be too deep
 //   I can't do anything about it, that's how Android apps are built.
 @SuppressWarnings("java:S110")
-public class ChangeLangActivity extends AppCompatActivity {
-    private static final Map<Integer, String> langMap;
+public class ChangeLangActivity extends LocalizationActivity {
+    private static final Map<Integer, String> langCodes;
 
     static {
-        langMap = new HashMap<>();
-        langMap.put(R.id.radio_en, "en");
-        langMap.put(R.id.radio_es, "es");
-        langMap.put(R.id.radio_fr, "fr");
+        langCodes = new HashMap<>();
+        langCodes.put(R.id.radio_en, "en");
+        langCodes.put(R.id.radio_es, "es");
+        langCodes.put(R.id.radio_fr, "fr");
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_language);
 
@@ -35,9 +35,8 @@ public class ChangeLangActivity extends AppCompatActivity {
         final AppCompatActivity activity = this;
         findViewById(R.id.btnChangeLangView).setOnClickListener(view -> {
             RadioGroup rg = findViewById(R.id.selected_lang);
-            LocaleHelper.setLocale(activity, langMap.get(rg.getCheckedRadioButtonId()));
-            // It is required to recreate the activity to reflect the change in UI.
-            recreate();
+            String langCode = langCodes.get(rg.getCheckedRadioButtonId());
+            setLanguage(langCode);
         });
     }
 }
