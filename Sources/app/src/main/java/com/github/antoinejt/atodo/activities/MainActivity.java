6a0f8a71d3212678cd5630dc1cac82bcb816e11f
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-01-31 02:27:54 +0100

    Address some linter issues

--- Sources/app/src/main/java/com/github/antoinejt/atodo/activities/MainActivity.java
+++ Sources/app/src/main/java/com/github/antoinejt/atodo/activities/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.akexorcist.localizationactivity.ui.LocalizationActivity;
@@ -59,9 +58,8 @@ public class MainActivity extends LocalizationActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        final int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Goto.changeActivity(this, ChangeLangActivity.class);
         }
